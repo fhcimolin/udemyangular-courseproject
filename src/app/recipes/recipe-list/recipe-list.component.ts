537de054ef64e288da1662ebaf57a64829397ae5
DEPSSC\flavio.simolin <flavio@deps.com.br>
2022-06-03 12:21:58 -0300

    Recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test','Decription',"https://lh3.googleusercontent.com/ogw/ADea4I4pVVqd3WE_fOJ2Q6k7ngg6zd6e8rhF4Q-dLqWHSw=s32-c-mo"),
+    new Recipe('Test','Decription',"https://lh3.googleusercontent.com/ogw/ADea4I4pVVqd3WE_fOJ2Q6k7ngg6zd6e8rhF4Q-dLqWHSw=s32-c-mo"),
+    new Recipe('Test','Decription',"https://lh3.googleusercontent.com/ogw/ADea4I4pVVqd3WE_fOJ2Q6k7ngg6zd6e8rhF4Q-dLqWHSw=s32-c-mo"),
+  ];
 
   constructor() { }
 
